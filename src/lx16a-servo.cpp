e3ab8e78f76a94c7cf653de4d92a6b81fbb986ad
Kevin Harrington <mad.hephaestus@gmail.com>
2020-08-14 17:07:27 -0400

    no prints

--- src/lx16a-servo.cpp
+++ src/lx16a-servo.cpp
@@ -26,36 +26,41 @@ bool LX16ABus::write_no_retry(uint8_t cmd, const uint8_t *params, int param_cnt,
 
 // clear input buffer
 	int junkCount = 0;
+	delayMicroseconds(timeus(2));
 	while (available()) {
 		if (junkCount == 0) {
-			if (_debug)
-				Serial.print("\n\t\t[ ");
+//			if (_debug)
+//				Serial.print("\n\t\t[ ");
 		}
-		if (_debug)
-			Serial.print(" " + String(read()) + ", ");
-		else
+//		if (_debug)
+//			Serial.print(" " + String(read()) + ", ");
+//		else
 			read();
 		delayMicroseconds(timeus(3));
 		junkCount++;
 	}
 
-	if (_debug && junkCount!=0) {
-		Serial.print("]\n ");
-		Serial.println(
-				"\t\t Junk bytes = " + String(junkCount) + " id " + String(MYID)
-						+ " cmd " + String(cmd) + " last cmd "
-						+ String(lastCommand));
-	}
+//	if (_debug && junkCount!=0) {
+//		Serial.print("]\n ");
+//		Serial.println(
+//				"\t\t Junk bytes = " + String(junkCount) + " id " + String(MYID)
+//						+ " cmd " + String(cmd) + " last cmd "
+//						+ String(lastCommand));
+//	}
 	lastCommand = cmd;
 	// send command packet
 	uint32_t t0 = millis();
 	if (myTXFlagGPIO >= 0) {
 		digitalWrite(myTXFlagGPIO, 1);
 	}
+	delayMicroseconds(timeus(1));
 	write(buf, buflen);
 	delayMicroseconds(timeus(1));
+	if (myTXFlagGPIO >= 0) {
+		digitalWrite(myTXFlagGPIO, 0);
+	}
 	// expect to read back command by virtue of single-pin loop-back
-	uint32_t tout = time(buflen) + 30; // 2ms margin
+	uint32_t tout = time(buflen+4) + 4; // 2ms margin
 	int got = 0;
 	bool ok = true;
 	if (_deepDebug)
@@ -73,96 +78,94 @@ bool LX16ABus::write_no_retry(uint8_t cmd, const uint8_t *params, int param_cnt,
 		ok = false;
 
 	}
-	if (_debug) {
-		if (!ok) {
-			Serial.print("\n\n\tWrote:[ ");
-			for(int i=0;i<buflen;i++){
-				Serial.print(" " + String(buf[i]) + ", ");
-			}
-			Serial.print("] id " + String(MYID)
-					+ " cmd " + String(cmd) + " last cmd "
-					+ String(lastCommand));
+//	if (_debug) {
+//		if (!ok) {
+//			Serial.print("\n\n\tWrote:[ ");
+//			for(int i=0;i<buflen;i++){
+//				Serial.print(" " + String(buf[i]) + ", ");
+//			}
+//			Serial.print("] id " + String(MYID)
+//					+ " cmd " + String(cmd) + " last cmd "
+//					+ String(lastCommand));
+//
+//			Serial.print("\n\tGot  :[ ");
+//			for(int i=0;i<got;i++){
+//				Serial.print(" " + String(ret[i]) + ", ");
+//			}
+//			Serial.print("] in "+String(millis()-t0)+"\n");
+//		}
+//	}
 
-			Serial.print("\n\tGot  :[ ");
-			for(int i=0;i<got;i++){
-				Serial.print(" " + String(ret[i]) + ", ");
-			}
-			Serial.print("]\n");
-		}
-	}
-	if (myTXFlagGPIO >= 0) {
-		digitalWrite(myTXFlagGPIO, 0);
-	}
 	return ok;
 }
 bool LX16ABus::rcv(uint8_t cmd, uint8_t *params, int param_len, uint8_t MYID) {
 	// read back the expected response
 	uint32_t t0 = millis();
-	uint32_t tout = time(param_len + 6) + 20; // 20ms for the servo to think
+	uint32_t tout = time(param_len + 6) + 30; // 20ms for the servo to think
 	int got = 0;
 	uint8_t sum = 0;
-	if (_deepDebug)
-		Serial.printf("RCV: ");
+//	if (_deepDebug)
+//		Serial.printf("RCV: ");
 	int len = 7; // minimum length
 	while (got < len && ((millis() - t0) < tout)) {
 		if (available()) {
 			int ch = read();
-			if (_deepDebug)
-				Serial.printf(" 0x%02x", ch);
+//			if (_deepDebug)
+//				Serial.printf(" 0x%02x", ch);
 			switch (got) {
 			case 0:
 			case 1:
 				if (ch != 0x55) {
-					if (_debug)
-						Serial.printf(" ERR (hdr)\n");
+//					if (_debug)
+//						Serial.printf(" ERR (hdr)\n");
 					return false;
 				}
 				break;
 			case 2:
 				if (ch != MYID && MYID != 0xfe) {
-					if (_debug)
-						Serial.printf(" ERR (id)\n");
+//					if (_debug)
+//						Serial.printf(" ERR (id)\n");
 					return false;
 				}
 				break;
 			case 3:
 				if (ch < 3 || ch > 7) {
-					if (_debug)
-						Serial.printf(" ERR (len)\n");
+//					if (_debug)
+//						Serial.printf(" ERR (len)\n");
 					return false;
 				}
 				len = ch + 3;
 				if (len > param_len + 6) {
-					if (_debug)
-						Serial.println(
-								" ERR (param_len) got " + String(len)
-										+ " expected "
-										+ String((param_len + 6)));
+//					if (_debug)
+//						Serial.println(
+//								" ERR (param_len) got " + String(len)
+//										+ " expected "
+//										+ String((param_len + 6)));
 					return false;
 				}
 				break;
 			case 4:
 				if (ch != cmd) {
-					if (_debug)
-						Serial.printf(" ERR (cmd)\n");
+//					if (_debug)
+//						Serial.printf(" ERR (cmd)\n");
 					return false;
 				}
 				break;
 			default:
 				if (got == len - 1) {
 					if ((uint8_t) ch == (uint8_t) ~sum) {
-						if (_deepDebug)
-							Serial.printf(" OK\n");
+//						if (_deepDebug)
+//							Serial.printf(" OK\n");
 						return true;
 					} else {
-						if (_debug)
-							Serial.printf(" ERR (cksum!=%02x)\n", ~sum);
+//						if (_debug)
+//							Serial.printf(" ERR (cksum!=%02x)\n", ~sum);
 						return false;
 					}
 				}
 				if (got - 5 > param_len) {
-					if (_debug)
-						Serial.printf(" ERR (cksum)\n");
+//					if (_debug)
+//						Serial.printf(" ERR (cksum)\n");
 					return false;
 				}
 				params[got - 5] = ch;
@@ -172,10 +175,14 @@ bool LX16ABus::rcv(uint8_t cmd, uint8_t *params, int param_len, uint8_t MYID) {
 			got++;
 		}
 	}
-	if (_debug)
-		Serial.println(
-				"Read TIMEOUT Expected " + String(len) + " got " + String(got)
-						+ " on cmd: " + String(cmd) + " id " + String(MYID));
+//	if (_debug){
+//		long done = millis();
+//		Serial.println(
+//				"Read TIMEOUT Expected " + String(len) + " got " + String(got)
+//
+//						+ " on cmd: " + String(cmd) + " id " + String(MYID)+
+//						" started at "+String(t0)+" finished at "+String (done)+" took "+String(done-t0));
+//	}
 	return false;
 }
 // read sends a command to the servo and reads back the response into the params buffer.
@@ -184,13 +191,13 @@ bool LX16ABus::read_no_retry(uint8_t cmd, uint8_t *params, int param_len,
 		uint8_t MYID) {
 	// send the read command
 	bool ok = write(cmd, NULL, 0, MYID);
-	if (!ok) {
-		if (_debug)
-			Serial.println(
-					"Command of read failed on cmd: " + String(cmd) + " id "
-							+ String(MYID));
-		return false;
-	}
+//	if (!ok) {
+//		if (_debug)
+//			Serial.println(
+//					"Command of read failed on cmd: " + String(cmd) + " id "
+//							+ String(MYID));
+//		return false;
+//	}
 
 	return rcv(cmd, params, param_len, MYID);
 }
